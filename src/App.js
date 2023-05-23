178ccf0329a294bbf13bfbf296075fa44d8d2e5d
GUchechi <godswilluchendu7@gmail.com>
2023-05-23 08:29:43 +0100

    movie

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
+
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App";
+// import { store } from "./features/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <Provider store={store}>
+  <Router>
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </Router>
+  // </Provider>
 );

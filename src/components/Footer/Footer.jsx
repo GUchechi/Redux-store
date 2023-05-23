3792405ad1010fe43a261e0f3334e38a0e2b5dd0
GUchechi <godswilluchendu7@gmail.com>
2023-05-23 08:44:33 +0100

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,13 @@
-import './Footer.scss'
+import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div>Movie App</div>
+      <div>©2023, Movie, Inc. or its affiliates</div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;

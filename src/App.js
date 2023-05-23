a80850a1807043ea64a9a1723974e1c3256b4c4e
GUchechi <godswilluchendu7@gmail.com>
2023-05-23 11:40:35 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import { Routes, Route } from "react-router-dom";
 import "./App.scss";
-import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
-import MovieDetail from "./components/MovieDetail/MovieDetail";
-import PageNotFound from "./components/PageNotFound/PageNotFound";
-import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-     Hello
-    </div>
-  );
+  return <div className="App">Hello</div>;
 }
 
 export default App;

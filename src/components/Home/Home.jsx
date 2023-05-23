4e79594fc440f30d6064fb4daab4a98b5b5e614d
GUchechi <godswilluchendu7@gmail.com>
2023-05-23 08:53:56 +0100

    refactoring

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,15 @@
+import MovieListing from '../MovieListing/MovieListing'
+import movieApi from '../../common/apis/movieApi'
+import {APIKey} from '../../common/apis/movieApiKey'
 import './Home.scss'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className="banner-img">
+        <MovieListing/>
+      </div>
+    </div>
   )
 }
 
